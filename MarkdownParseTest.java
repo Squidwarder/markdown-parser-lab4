0e0d6990f70388f8b5c3743ef61df0c5278f80ba
SeaSponge <2402578988@qq.com>
2022-07-07 09:17:31 -0700

    Added more tests

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -30,6 +30,21 @@ public class MarkdownParseTest {
 
 
     @Test
+    public void testGetLinks2() throws IOException {
+
+        Path fileName = Path.of("test-file2.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("https://google.com");
+        expected.add("some-thing.html");
+        assertEquals(expected, links);
+
+    }
+
+
+    @Test
     public void testGetLinks3() throws IOException {
 
         Path fileName = Path.of("test-file3.md");
@@ -38,6 +53,18 @@ public class MarkdownParseTest {
 
         ArrayList<String> expected = new ArrayList<>();
         expected.add("more text here");
+        assertEquals(expected, links);
+
+    }
+
+    @Test
+    public void testGetLinks4() throws IOException {
+
+        Path fileName = Path.of("test-file4.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+
+        ArrayList<String> expected = new ArrayList<>();
         assertEquals(expected, links);
 
     }
@@ -54,6 +81,48 @@ public class MarkdownParseTest {
         expected.add("page.com");
         assertEquals(expected, links);
 
+    }
+
+    @Test
+    public void testGetLinks6() throws IOException {
+
+        Path fileName = Path.of("test-file6.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("page.com");
+        assertEquals(expected, links);
+
+    }
+
+    @Test
+    public void testGetLinks7() throws IOException {
+
+        Path fileName = Path.of("test-file7.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+
+        ArrayList<String> expected = new ArrayList<>();
+        assertEquals(expected, links);
+
+    }
+
+    @Test
+    public void testGetLinks8() throws IOException {
+
+        Path fileName = Path.of("test-file8.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("a link on the first line");
+        assertEquals(expected, links);
+
+    }
+
+    public static void main(String[] args) {
+        return;
     }
 
 }
